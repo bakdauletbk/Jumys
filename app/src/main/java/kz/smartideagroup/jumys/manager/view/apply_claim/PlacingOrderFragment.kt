0a72e553ae49654497f99f316ac6e997ceef94ca
ErmahanOmir <erkow_0501@mail.ru>
2021-05-05 18:39:56 +0600

    sdfsd

---
+++ app/src/main/java/kz/smartideagroup/jumys/manager/view/apply_claim/PlacingOrderFragment.kt
@@ -0,0 +1,17 @@
+package kz.smartideagroup.jumys.manager.view.apply_claim
+
+import android.os.Bundle
+import android.view.View
+import kz.smartideagroup.jumys.R
+import kz.smartideagroup.jumys.common.views.BaseFragment
+
+class PlacingOrderFragment:BaseFragment(R.layout.fragment_placing_order) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lets()
+    }
+
+    private fun lets() {
+    }
+}
