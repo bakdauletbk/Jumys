a1608bab35c8dc7e20d8ec44dc82a5ff9cd27319
ErmahanOmir <erkow_0501@mail.ru>
2021-05-13 11:17:42 +0600

    add page

---
+++ app/src/main/java/kz/smartideagroup/jumys/client/view/about_app/AboutAppFragment.kt
@@ -0,0 +1,33 @@
+package kz.smartideagroup.jumys.client.view.about_app
+
+import android.os.Bundle
+import android.view.View
+import kz.smartideagroup.jumys.R
+import kz.smartideagroup.jumys.common.views.BaseFragment
+
+class AboutAppFragment : BaseFragment(R.layout.fragment_about_app) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lets()
+    }
+
+    private fun lets() {
+        initViewModel()
+        initListeners()
+        initObservers()
+    }
+
+    private fun initViewModel() {
+
+    }
+
+    private fun initListeners() {
+
+    }
+
+    private fun initObservers() {
+
+    }
+
+}
